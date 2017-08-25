91eefe6f2234c08bd7ed430c073d3c562ccaa601
mcanzerini <mac09@MacBook-Pro.local>
2017-08-25 14:34:36 +0200

    Feat(distribution)

--- given-care-back/lib/plugin/project/index.js
+++ given-care-back/lib/plugin/project/index.js
@@ -5,6 +5,7 @@ const register = function (server, options, next) {
     server.route(require('./route/id.route'));
     server.route(require('./route/project.route'));
     server.route(require('./route/rate.route'));
+    server.route(require('./route/distribution.route'));
 
     return next();
 };
