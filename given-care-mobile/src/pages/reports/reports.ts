8b0530bfa8a03e3d67d7ee9f423338a4f67f4eda
Nicolas Andre <nicolas.andre@intech.lu>
2017-08-26 10:44:55 +0200

    evol(add image in reporting)

--- given-care-mobile/src/pages/reports/reports.ts
+++ given-care-mobile/src/pages/reports/reports.ts
@@ -3,6 +3,7 @@ import { NavController, NavParams } from 'ionic-angular';
 import {HomePage} from "../home/home";
 import { ReportService } from '../../services/report.service';
 import { AlertController } from 'ionic-angular';
+import { ViewChild } from '@angular/core';
 
 @Component({
   selector: 'page-reports',
@@ -11,6 +12,18 @@ import { AlertController } from 'ionic-angular';
 export class ReportsPage {
 
   emergency=false;
+  cameraOpen=false;
+  photoTaken=false;
+  videoSrc='';
+  type='';
+
+  @ViewChild('videoPlayer') videoplayer: any;
+  @ViewChild('canvasImg') photo: any;
+
+
+  toggleVideo() {
+    this.videoplayer.nativeElement.play();
+  }
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public reportService : ReportService,
               public alertCtrl:AlertController) {
@@ -24,28 +37,59 @@ export class ReportsPage {
     }, 3000);
   }
 
-  sendReport(type:string){
+  openCamera(){
+    this.cameraOpen = true;
+    // Grab elements, create settings, etc.
+
+    const self = this;
+    // Get access to the camera!
+    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      // Not adding `{ audio: true }` since we only want video now
+      navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+        self.videoSrc = window.URL.createObjectURL(stream);
+        self.toggleVideo();
+      });
+    }
+  }
+
+  snap(){
+    this.photoTaken = true;
+
+    let context:any = this.photo.nativeElement.getContext('2d');
+
+    context.drawImage(this.videoplayer.nativeElement, 0, 0, 640, 480);
+  }
+
+
+  prepareReport(type:string){
+    this.type = type;
+    this.openCamera();
+  }
+
+  sendReport(){
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition((position) => {
-        return this.justSendReport({type, gps : {latitude : position.coords.latitude, longitude : position.coords.longitude}})
+        return this.justSendReport({type : this.type, gps : {latitude : position.coords.latitude, longitude : position.coords.longitude}})
       });
 
     } else {
       // geolocation not available
-      return this.justSendReport({type, gps : {latitude : 0, longitude : 0}})
+      return this.justSendReport({type : this.type, gps : {latitude : 0, longitude : 0}})
     }
-
   }
 
 
 
   justSendReport(obj){
+    const self = this;
     this.reportService.send(obj)
       .then(
         (response:any) => {
           const data = response.json();
           this.showOkAlert(data.totalXP, data.gain);
-
+          setTimeout(() => {
+            self.navCtrl.setRoot(HomePage);
+          }, 5000);
         },
         (err) => {
           console.log("failed");
